40f85fbf80c9810dfe4465f3842df0e54278abcb
Christian Monaghan <christian.monaghan@pm.me>
2022-05-18 12:16:13 -0400

    rudimentary method of calculating dino equivalents for emissions

--- src/helpers.js
+++ src/helpers.js
@@ -1,22 +1,59 @@
-// list of some common dinosaurs: Tyrannosaurus Rex, Triceratops, Velociraptor, Stegosaurus, Spinosaurus, Archaeopteryx, Brachiosaurus, Pterodactyl, Allosaurus
-const dinoData = [
+// list of some common dinosaurs: Tyrannosaurus Rex, Triceratops, Velociraptor, Stegosaurus, Spinosaurus, Archaeopteryx, Brachiosaurus, Pterodactyl, Allosaurus, Megalodon
+const dinoConstantsDescendingOrder = [
   {
     name: "Tyrannosaurus Rex",
-    count: 3,
+    emissionsPerDino: .8,
     image: "https://webstockreview.net/images/clipart-dinosaur-silhouette-15.png",
   },
   {
+    name: "Brachiosaurus",
+    emissionsPerDino: .65,
+  },
+  {
+    name: "Megalodon",
+    emissionsPerDino: .47,
+  },
+  {
     name: "Triceratops",
-    count: 1,
+    emissionsPerDino: .2,
     image: "https://designlooter.com/images/herbivorous-svg-3.png",
   },
   {
     name: "Pterodactyl",
-    count: 4,
+    emissionsPerDino: .01,
     image: "https://cdn3.iconfinder.com/data/icons/prehistoric-animals/100/05-512.png",
-  },
-];
+  }
+]
 
+
+// returns data in the format:
+//   const dinoData = [
+//     {
+//       name: "Tyrannosaurus Rex",
+//       count: 3,
+//       image: "https://webstockreview.net/images/clipart-dinosaur-silhouette-15.png",
+//     },
+//     ...
+//   }
+// all emissions values assume tons CO2 equivalent
 export const covertEmissionsToDinos = (emissionsValue) => {
+  let remainingValue = emissionsValue;
+  let dinoData = [];
+
+  dinoConstantsDescendingOrder.forEach(dino => {
+    let maxDinos = Math.floor(remainingValue / dino.emissionsPerDino);
+
+    if (maxDinos > 0) {
+      dinoData.push({
+        name: dino.name,
+        count: maxDinos,
+        image: dino.image,
+      });
+
+      // decrement remaining value so we know when all emissions are allocated
+      remainingValue = remainingValue - (maxDinos * dino.emissionsPerDino);
+    }
+  });
+
   return dinoData;
 }
