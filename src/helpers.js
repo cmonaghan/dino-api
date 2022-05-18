21386cfb5b9a678444b46a07dffbe413af2bf479
Christian Monaghan <christian.monaghan@pm.me>
2022-05-18 11:50:14 -0400

    Add error response; refactor calculation into helpers file

---
+++ src/helpers.js
@@ -0,0 +1,22 @@
+// list of some common dinosaurs: Tyrannosaurus Rex, Triceratops, Velociraptor, Stegosaurus, Spinosaurus, Archaeopteryx, Brachiosaurus, Pterodactyl, Allosaurus
+const dinoData = [
+  {
+    name: "Tyrannosaurus Rex",
+    count: 3,
+    image: "https://webstockreview.net/images/clipart-dinosaur-silhouette-15.png",
+  },
+  {
+    name: "Triceratops",
+    count: 1,
+    image: "https://designlooter.com/images/herbivorous-svg-3.png",
+  },
+  {
+    name: "Pterodactyl",
+    count: 4,
+    image: "https://cdn3.iconfinder.com/data/icons/prehistoric-animals/100/05-512.png",
+  },
+];
+
+export const covertEmissionsToDinos = (emissionsValue) => {
+  return dinoData;
+}
--- src/index.js
+++ src/index.js
@@ -1,35 +1,32 @@
 import url from 'url';
+import {
+  covertEmissionsToDinos,
+} from './helpers.js';
+
 
 addEventListener('fetch', event => {
   let requestUrl = url.parse(event.request.url, true);
-  console.log(requestUrl.query);
 
-  // list of some common dinosaurs: Tyrannosaurus Rex, Triceratops, Velociraptor, Stegosaurus, Spinosaurus, Archaeopteryx, Brachiosaurus, Pterodactyl, Allosaurus
-  const dinoData = [
-    {
-      name: "Tyrannosaurus Rex",
-      count: 3,
-      image: "https://webstockreview.net/images/clipart-dinosaur-silhouette-15.png",
-    },
-    {
-      name: "Triceratops",
-      count: 1,
-      image: "https://designlooter.com/images/herbivorous-svg-3.png",
-    },
-    {
-      name: "Pterodactyl",
-      count: 4,
-      image: "https://cdn3.iconfinder.com/data/icons/prehistoric-animals/100/05-512.png",
-    },
-  ];
+  const query = requestUrl.query
+  console.log(query);
 
-  const json = JSON.stringify(dinoData, null, 2);
+  if (query && query.emissions) {
+    let dinoData = covertEmissionsToDinos(query.emissions);
 
-  return event.respondWith(
-    new Response(json, {
-      headers: {
-        'content-type': 'application/json;charset=UTF-8',
-      },
-    })
-  );
+    const json = JSON.stringify(dinoData, null, 2);
+
+    return event.respondWith(
+      new Response(json, {
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+      })
+    );
+  } else {
+    return event.respondWith(
+      new Response("Error! Requests must include a query param in the format '?emissions={number}'", {
+        status: 400,
+      })
+    );
+  }
 });
