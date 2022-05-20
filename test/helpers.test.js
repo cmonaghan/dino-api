0323c0cdb09c8ea8729f993afd0d4551988ff44b
Christian Monaghan <christian.monaghan@pm.me>
2022-05-19 20:10:07 -0400

    add tests for first helper function

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -7,5 +7,20 @@ describe("convertEmissionsToDinos", () => {
     expect(convertEmissionsToDinos).toBeTruthy();
   });
 
+  test("returns array", () => {
+    let result = convertEmissionsToDinos(1);
+    const isArray = Array.isArray(result);
+    expect(isArray).toBe(true);
+  });
+
+  test("array contains object with expected properties", () => {
+    let result = convertEmissionsToDinos(1);
+    let firstDino = result[0];
+
+    expect(firstDino.hasOwnProperty("name")).toBe(true);
+    expect(firstDino.hasOwnProperty("count")).toBe(true);
+    expect(firstDino.hasOwnProperty("image")).toBe(true);
+  });
+
   test.todo("another test");
 });
